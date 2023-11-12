9fd104e32a244846fc1bb480a1cb52728eb28c96
Liandra Whittaker <liawhitt200@gmail.com>
2023-11-12 14:27:54 -0500

    Exercise 1 has been completed

---
+++ app.js
@@ -0,0 +1,29 @@
+
+window.onload =function(){
+    var btn = document.querySelector("#button");
+    var httpRequest;
+
+    btn.addEventListener('click', function(element){
+        element.preventDefault();
+
+        httpRequest=new XMLHttpRequest();
+
+        var url="superheroes.php";
+        httpRequest.onreadystatechange = showheroes;
+        httpRequest.open('GET',url);
+        httpRequest.send();
+    });
+
+    function showheroes(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+              var response = httpRequest.responseText;
+                alert(response);
+            } else {
+              alert('There was a problem with the request.');
+            }
+          }
+        }
+    };
+
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="width-device-width, initial scale=1.0"
+        <title><h1>Avengers Character Search</h1></title>
+        <link rel="stylesheet" href="styles.css">
+        <script src="app.js"></script>
+    </head>
+    <body>
+        <button type="submit" id= "button" class="btn" > Search </button>
+    
+        <div id="names"></div>
+    </body>
+</html>
+   
